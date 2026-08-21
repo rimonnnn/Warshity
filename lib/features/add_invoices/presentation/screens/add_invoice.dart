6af401a77145baa62774f4d0928e868efe385ca2
kero12-eg <madk5078@gmail.com>
2026-08-21 17:36:34 +0300

    Solve Category Cubit error

--- lib/features/add_invoices/presentation/screens/add_invoice.dart
+++ lib/features/add_invoices/presentation/screens/add_invoice.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:warshity/core/di/injection.dart';
 import 'package:warshity/core/widgets/app_responsive.dart';
 import 'package:warshity/features/add_invoices/presentation/layout/mobile_add_invoice.dart';
 import 'package:warshity/features/add_invoices/presentation/layout/web_add_invoice.dart';
+import 'package:warshity/features/products/presentation/cubit/categories_cubit.dart';
 
 class AddInvoice extends StatelessWidget {
   const AddInvoice({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AppResponsive(mobile: MobileAddInvoice(), desktop: WebAddInvoice());
+    return BlocProvider<CategoriesCubit>(
+      create: (_) => getIt<CategoriesCubit>()..watchCategories(),
+      child: AppResponsive(mobile: MobileAddInvoice(), desktop: WebAddInvoice()),
+    );
   }
 }
