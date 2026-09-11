108f0cb003123704b8d98df0f590d6707a53557f
rimonnnn <rimonseha123@gmail.com>
2026-09-12 01:34:39 +0300

    delete refreshIndicator from invoice history screen

--- lib/features/invoices/presentation/layout/invoice_mobile.dart
+++ lib/features/invoices/presentation/layout/invoice_mobile.dart
@@ -31,21 +31,16 @@ class InvoiceMobile extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 8.w),
-          child: RefreshIndicator(
-            onRefresh: () async {
-              await Future.delayed(const Duration(seconds: 300));
-            },
-            child: Column(
-              children: [
-                const InvoiceSearchBar(),
-                HeightSpace(8),
-                const InvoiceStatisticsSection(),
-                HeightSpace(8),
-                const InvoiceFilterChips(),
-                HeightSpace(24),
-                const Expanded(child: InvoiceList()),
-              ],
-            ),
+          child: Column(
+            children: [
+              const InvoiceSearchBar(),
+              HeightSpace(8),
+              const InvoiceStatisticsSection(),
+              HeightSpace(8),
+              const InvoiceFilterChips(),
+              HeightSpace(24),
+              const Expanded(child: InvoiceList()),
+            ],
           ),
         ),
       ),
